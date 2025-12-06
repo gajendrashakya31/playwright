a4d63062ca67fc11274623ac57dcbaca38543ac7
gajendrashakya31 <gajendrashakya31@gmail.com>
2025-12-06 08:37:57 +0530

    add more steps

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -6,5 +6,8 @@ test('open amazon.com and check title', async ({ page }) => {
   await page.getByPlaceholder('Search Amazon').fill('mobile');
   await page.getByPlaceholder('Search Amazon').press('Enter');
   await expect(page).toHaveTitle(/mobile/);
+  let links = await page.getByRole('link').all();
+  console.log(`Total links found: ${links.length}`);
+  await page.getByRole('link', { name: "Today's Deals" }).click();
   await page.close();
 });
